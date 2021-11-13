081c5cf922b0e5e3fd436c3cd705688d20954dca
Runchen Tao <runchentao@gmail.com>
2021-11-12 19:32:56 -0800

    update nav slider and layout

--- portfolioout/src/App.js
+++ portfolioout/src/App.js
@@ -1,26 +1,55 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Row, Col } from "antd";
+import { Layout, Breadcrumb, Row, Col } from "antd";
 
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 const App = () => {
+  const { Header, Content, Footer, Sider } = Layout;
+
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
+
   return (
     <Router>
-      <div className="App">
-        <Row>
+      <div>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+            <Navigation />
+          </Sider>
+          <Layout className="site-layout">
+            <Header className="site-layout-background" style={{ padding: 0 }} />
+            <Content style={{ margin: "0 16px" }}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/portfolio" element={<Home />} />
+              </Routes>
+              {/* <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ padding: 24, minHeight: 360 }}>Bill is a cat.</div> */}
+            </Content>
+            <Footer style={{ textAlign: "center" }}>Portfolioout ©2021</Footer>
+          </Layout>
+        </Layout>
+
+        {/* <Row>
           <Col>
             <Navigation />
           </Col>
           <Col>
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/portfolio" element={<Home />} />
             </Routes>
           </Col>
-        </Row>
+        </Row> */}
       </div>
     </Router>
   );
--- portfolioout/src/components/Navigation/Navigation.js
+++ portfolioout/src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 import { Menu, Switch } from "antd";
 import Item from "antd/lib/list/Item";
 
@@ -32,32 +33,33 @@ const Navigation = () => {
   return (
     <>
       <div className={darkMode ? "container darkMode" : "container"}>
-        <div>
-          <Menu
-            theme={darkMode ? "dark" : "light"}
-            onClick={handleClick}
-            style={{ width: 256 }}
-            defaultOpenKeys={["sub1"]}
-            defaultSelectedKeys={["1"]}
-            selectedKeys={[current]}
-            mode="inline"
-          >
-            <SubMenu key="sub1" icon={<MailOutlined />} title="Dashboard">
-              <Menu.Item key="My Portfolio">My Portfolio</Menu.Item>
-              <Menu.Item key="Weekly Summary">Weekly Summary</Menu.Item>
+        <img className="app-logo" src={logo} alt="" />
+        <Menu
+          theme={darkMode ? "dark" : "light"}
+          onClick={handleClick}
+          defaultOpenKeys={["sub1"]}
+          defaultSelectedKeys={["1"]}
+          selectedKeys={[current]}
+          mode="inline"
+        >
+          <SubMenu key="sub1" icon={<MailOutlined />} title="Dashboard">
+            <Menu.Item key="My Portfolio">
+              <Link to="/portfolio">My Portfolio</Link>
+            </Menu.Item>
+            <Menu.Item key="Weekly Summary">Weekly Summary</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Invest">
+            <Menu.Item key="Ethical Investing">Ethical Investing</Menu.Item>
+            <Menu.Item key="Growth Investing">Growth Investing</Menu.Item>
+            <Menu.Item key="Index Investing">Index Investing</Menu.Item>
+            <Menu.Item key="Quality Investing">Quality Investing</Menu.Item>
+            <Menu.Item key="Value Investing">Value Investing</Menu.Item>
+            <SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="8">Option 7</Menu.Item>
+              <Menu.Item key="9">Option 8</Menu.Item>
             </SubMenu>
-            <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Invest">
-              <Menu.Item key="Ethical Investing">Ethical Investing</Menu.Item>
-              <Menu.Item key="Growth Investing">Growth Investing</Menu.Item>
-              <Menu.Item key="Index Investing">Index Investing</Menu.Item>
-              <Menu.Item key="Quality Investing">Quality Investing</Menu.Item>
-              <Menu.Item key="Value Investing">Value Investing</Menu.Item>
-              <SubMenu key="sub3" title="Submenu">
-                <Menu.Item key="8">Option 7</Menu.Item>
-                <Menu.Item key="9">Option 8</Menu.Item>
-              </SubMenu>
-            </SubMenu>
-            {/* <SubMenu key="sub4" icon={<SettingOutlined />} title="Settings">
+          </SubMenu>
+          {/* <SubMenu key="sub4" icon={<SettingOutlined />} title="Settings">
               <Item key="9" className="darkModeSwitch">
                 Dark Mode
                 <Switch
@@ -67,9 +69,9 @@ const Navigation = () => {
                 />
               </Item>
             </SubMenu> */}
-          </Menu>
-        </div>
-        <img className="logo" src={logo} alt="" />
+        </Menu>
+
+        {/* <img className="logo" src={logo} alt="" /> */}
       </div>
     </>
   );
