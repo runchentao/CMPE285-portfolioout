1e4e5b7f2b6ac5e8afa5d438fa1791fa61e87868
Runchen Tao <runchentao@gmail.com>
2021-12-10 10:31:35 -0800

    test get route ok

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from main import *
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,7 +8,9 @@ api = Api(app)
 
 class StockSuggestion(Resource):
     def get(self):
-        return {'data': 'GET route hit'}, 200
+        s = StockAPI()
+        status = s.createPortfolio(5, 5000)
+        return {'data': s.portfolio}, 200 if status == 1 else 500
 
     def post(self):
         return {'data': 'POST route hit'}, 200
