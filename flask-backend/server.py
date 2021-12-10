5c3d1ab91b38cc6156070f4c0df1eaae9e08a4f0
Runchen Tao <runchentao@gmail.com>
2021-12-10 10:53:34 -0800

    setup GET reqeust

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_restful import Resource, Api
 from main import *
 
@@ -7,16 +7,16 @@ api = Api(app)
 
 
 class StockSuggestion(Resource):
-    def get(self):
-        s = StockAPI()
-        status = s.createPortfolio(5, 5000)
-        return {'data': s.portfolio}, 200 if status == 1 else 500
+    def get(self, strategy, investment):
+        try:
+            s = StockAPI()
+            status = s.createPortfolio(strategy, investment)
+            return {'data': s.portfolio}, 200 if status == 1 else abort(400)
+        except:
+            abort(500)
 
-    def post(self):
-        return {'data': 'POST route hit'}, 200
 
-
-api.add_resource(StockSuggestion, '/')
+api.add_resource(StockSuggestion, '/suggest/<int:strategy>/<int:investment>')
 
 if __name__ == '__main__':
     app.run(debug=True)
