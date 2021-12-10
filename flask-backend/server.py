eaf08635c7f238ddf832b0d85384d9b4ee8fc389
Runchen Tao <runchentao@gmail.com>
2021-12-10 10:18:50 -0800

    add flask-restful to server.py

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class StockSuggestion(Resource):
+    def get(self):
+        return {'data': 'GET route hit'}, 200
+
+    def post(self):
+        return {'data': 'POST route hit'}, 200
+
+
+api.add_resource(StockSuggestion, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
