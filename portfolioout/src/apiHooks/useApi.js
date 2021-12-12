ec4340a148ff6bc86ea84d2a9bdd119a2ca9a713
Runchen Tao <runchentao@gmail.com>
2021-12-11 23:04:17 -0800

    update api hook

--- portfolioout/src/apiHooks/useApi.js
+++ portfolioout/src/apiHooks/useApi.js
@@ -4,13 +4,19 @@ import axios from "axios";
 const useApi = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
+  const [chartData, setChartData] = useState(null);
+  const [portfolioData, setPortfolioData] = useState(null);
+  const [pieData, setPieData] = useState(null);
 
   const fetchApi = async (url) => {
     setLoading(true);
     try {
       await axios.get(url).then((res) => {
-        const { data } = res.data;
+        const { data, chartData, portfolio, pieData } = res.data;
         setData(data);
+        setChartData(chartData);
+        setPortfolioData(portfolio);
+        setPieData(pieData);
         setLoading(false);
       });
     } catch (err) {
@@ -19,7 +25,7 @@ const useApi = () => {
     }
   };
 
-  return { loading, data, fetchApi };
+  return { loading, data, chartData, portfolioData, pieData, fetchApi };
 };
 
 export default useApi;
